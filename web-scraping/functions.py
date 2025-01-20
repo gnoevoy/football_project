4c36564fe00f3aa0afc5d3bdc0437522e81f61a5
andrey_gnoevoy <gnoevoyandrey044@gmail.com>
2025-01-20 23:00:59 +0100

    worked code for scraping links

---
+++ web-scraping/functions.py
@@ -0,0 +1,43 @@
+from playwright.sync_api import sync_playwright, Playwright
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+def handle_cookies(page):
+    cookie = page.locator("div.CybotCookiebotDialogActive")
+    cookie.wait_for()
+    page.click("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+
+
+def get_total_items(page):
+    html = page.content()
+    soup = BeautifulSoup(html, "html.parser")
+    total = soup.find("p", class_="products-list-controls-container__paragraph amount")
+    return int(total.text)
+
+
+def get_product_links(page):
+    html = page.content()
+    soup = BeautifulSoup(html, "html.parser")
+    grid = soup.find("div", class_="category-grid category-grid--small-items")
+    products = grid.select("div.category-grid__item:not(.last-brick)")
+    links = grid.find_all("a", class_=re.compile(r"\bproduct-item product-brick\b"))
+    page_links = [link["href"] for link in links]
+
+    return page_links, products
+
+
+def pagination(page, url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        page.goto(url)
+        page.wait_for_selector("div.category-grid__item")
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    pass
--- web-scraping/plan.md
+++ web-scraping/plan.md
@@ -5,6 +5,7 @@ Result: get structured 2 files (json or csv) and 2 folders with content images
 
 shoes: https://www.r-gol.com/en/football-boots?filters=131%5B7502%5D%7e135%5B7586%2C7547%2C7504%2C8212%2C7617%5D%7e152%5B83011%5D
 balls: https://www.r-gol.com/en/footballs?filters=131%5B83115%5D%7e135%5B7586%2C7547%2C7504%2C19117%5D
+
 - title
 - price
 - rgol id
@@ -17,13 +18,6 @@ balls: https://www.r-gol.com/en/footballs?filters=131%5B83115%5D%7e135%5B7586%2C
 ===================
 
 plan
-1. create a file with:
-    - total vs actual
-    - page | name | link
--> identify a problem
 
 2. separate code by function and blocks (perhaps severals files: functions, actual code and tranformation)
 3. handle product page data
-
-
-
